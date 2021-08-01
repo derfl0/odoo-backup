73294d3692d28b8fb3c3463a5d3547c2ead1e031
Flo Bieringer <wertfrei@icloud.com>
2021-08-01 13:44:29 +0200

    Add option to restore after backup

--- backup.sh
+++ backup.sh
@@ -3,14 +3,45 @@
 echo "Starting backup at $(date)"
 
 # create a backup
+BACKUP_NAME=${BACKUP_DIR}/${ODOO_DATABASE}.$(date +%F_%H%M%S).zip
 curl -s -X POST \
     -F "master_pwd=${ADMIN_PASSWORD}" \
     -F "name=${ODOO_DATABASE}" \
     -F "backup_format=zip" \
-    -o ${BACKUP_DIR}/${ODOO_DATABASE}.$(date +%F_%H%M%S).zip \
+    -o ${BACKUP_NAME} \
     ${URL}/web/database/backup
 
 # delete old backups
-find ${BACKUP_DIR} -type f -mtime +8 -name "${ODOO_DATABASE}.*.zip" -delete
+if [ -v ${DELETE_AFTER} ] ; then
+    echo "Deleting old backups"
+    find ${BACKUP_DIR} -type f -mtime +${DELETE_AFTER} -name "${ODOO_DATABASE}.*.zip" -delete
+fi
 
 echo "Finished backup"
+
+# If we want to restore the database as well
+if [ -v ${RESTORE_URL} ] ; then
+
+    echo "Restoring backup"
+
+    # find latest backup
+    #BACKUP_FILE=$(ls -t ${BACKUP_DIR} | head -1)
+    #BACKUP_PATH="${BACKUP_DIR}/${BACKUP_FILE}"
+
+    # drop previous db
+    curl -s -X POST \
+        -F "master_pwd=${RESTORE_ADMIN_PASSWORD}" \
+        -F "name=${RESTORE_DATABASE}" \
+        ${RESTORE_URL}/web/database/drop > /dev/null
+
+    # restore backup
+    curl -s -X POST \
+        -F "master_pwd=${RESTORE_ADMIN_PASSWORD}" \
+        -F backup_file=@${BACKUP_NAME} \
+        -F 'copy=true' \
+        -F "name=${RESTORE_DATABASE}" \
+        ${RESTORE_URL}/web/database/restore > /dev/null
+
+    echo "Restoring backup completed"
+
+fi
--- readme.md
+++ readme.md
@@ -8,17 +8,20 @@ This docker container automates the download of an Odoo backup via the Odoo DB M
 
 ## Environment
 
-**BACKUP_DIR**
-Rewrite the backup directory inside the container
+| EnvVar | Description |
+| --- | --- |
+| BACKUP_DIR | Rewrite the backup directory inside the container |
+| ODOO_DATABASE | Define the name of the Odoo database to backup |
+| ADMIN_PASSWORD | Masterpassword of the instance  |
+| URL | URL where the instance is available |
+| SCHEDULE | Cronscheduler for the backup (e.g. */30 * * * * ) |
+| DELETE_AFTER | Delete old backups after **n** days |
 
-**ODOO_DATABASE**
-Define the name of the Odoo database to backup
+## Restore after backup
+If you have a staging environment and want to restore the captured backup once the job is finished, use:
 
-**ADMIN_PASSWORD**
-Masterpassword of the instance
-
-**URL**
-URL where the instance is available
-
-**SCHEDULE**
-Cronscheduler for the backup (e.g. */30 * * * * )
+| EnvVar | Description |
+| --- | --- |
+| RESTORE_URL | URL of the staging environment |
+| RESTORE_DATABASE | The target database for the restore. **DANGER!** the previous database will be dropped automatically **without** a backup. |
+| RESTORE_ADMIN_PASSWORD | Admin password for the url |
