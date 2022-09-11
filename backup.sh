fab6748d8b20e0a226b148a80ad6a746956c181d
Flo Bieringer <wertfrei@icloud.com>
2022-09-11 11:23:40 +0200

    fix backup

--- backup.sh
+++ backup.sh
@@ -12,52 +12,7 @@ function create_web_backup() {
         ${URL}/web/database/backup
 }
 
-function create_db_backup() {
-    echo "Backup via SQL access"
-
-    export PGPASSWORD="${DB_PASSWORD}"
-
-    mkdir ${BACKUP_FOLDER_NAME}
-
-    pg_dump \
-        --user ${DB_USER} \
-        --host ${DB_HOST} \
-        ${ODOO_DATABASE} > ${BACKUP_FOLDER_NAME}/dump.sql
-
-    if [ -d "$FILESTORE_DIR/filestore/$ODOO_DATABASE" ]; then
-        ln -s "$FILESTORE_DIR/filestore/$ODOO_DATABASE" ${BACKUP_FOLDER_NAME}/filestore
-    else
-        if [ -d "$FILESTORE_DIR/$ODOO_DATABASE" ]; then
-            ln -s "$FILESTORE_DIR/$ODOO_DATABASE" ${BACKUP_FOLDER_NAME}/filestore
-        fi
-    fi
-
-    zip -rq ${BACKUP_NAME} ${BACKUP_FOLDER_NAME}
-
-    rm -rf ${BACKUP_FOLDER_NAME}
-}
-
-echo "Starting backup at $(date)"
-BACKUP_FOLDER_NAME=${ODOO_DATABASE}.$(date +%F_%H%M%S)
-BACKUP_NAME=${BACKUP_DIR}/${ODOO_DATABASE}.$(date +%F_%H%M%S).zip
-
-# check if web or sql backup
-if [ -z "$DB_HOST" ]; then
-    create_web_backup
-else
-    create_db_backup
-fi
-
-echo "Finished backup"
-
-# delete old backups
-if [ ! -z "$DELETE_AFTER" ]  && [ "$DELETE_AFTER" -gt 0 ] ; then
-    echo "Deleting old backups"
-    find ${BACKUP_DIR} -type f -mtime +${DELETE_AFTER} -name "${ODOO_DATABASE}.*.zip" -delete
-fi
-
-# If we want to restore the database as well
-if [ ! -z "$RESTORE_URL" ] ; then
+function restore_web_backup() {
 
     echo "Restoring backup"
 
@@ -84,5 +39,62 @@ if [ ! -z "$RESTORE_URL" ] ; then
         ${RESTORE_URL}/web/database/restore
 
     echo "Restoring backup completed"
+}
 
+function create_db_backup() {
+    echo "Backup via SQL access"
+
+    export PGPASSWORD="${DB_PASSWORD}"
+
+    mkdir ${BACKUP_FOLDER_NAME}
+
+    pg_dump \
+        --user ${DB_USER} \
+        --host ${DB_HOST} \
+        ${ODOO_DATABASE} > ${BACKUP_FOLDER_NAME}/dump.sql
+
+    if [ -d "$FILESTORE_DIR/filestore/$ODOO_DATABASE" ]; then
+        ln -s "$FILESTORE_DIR/filestore/$ODOO_DATABASE" ${BACKUP_FOLDER_NAME}/filestore
+    elif [ -d "$FILESTORE_DIR/$ODOO_DATABASE" ]; then
+        ln -s "$FILESTORE_DIR/$ODOO_DATABASE" ${BACKUP_FOLDER_NAME}/filestore
+    elif [ -d "$FILESTORE_DIR/.local/share/Odoo/filestore/$ODOO_DATABASE" ]; then
+        ln -s "$FILESTORE_DIR/.local/share/Odoo/filestore/$ODOO_DATABASE" ${BACKUP_FOLDER_NAME}/filestore
+    fi
+
+    zip -rq ${BACKUP_NAME} ${BACKUP_FOLDER_NAME}
+
+    rm -rf ${BACKUP_FOLDER_NAME}
+}
+
+function restore_db_backup {
+
+}
+
+echo "Starting backup at $(date)"
+BACKUP_FOLDER_NAME=${ODOO_DATABASE}.$(date +%F_%H%M%S)
+BACKUP_NAME=${BACKUP_DIR}/${ODOO_DATABASE}.$(date +%F_%H%M%S).zip
+
+# check if direct db backup
+if [ ! -z "$DB_HOST" ]; then
+    create_db_backup
+fi
+
+# check if web backup
+if [ ! -z "$URL" ]; then
+    create_web_backup
+fi
+
+# delete old backups
+if [ ! -z "$DELETE_AFTER" ]  && [ "$DELETE_AFTER" -gt 0 ] ; then
+    echo "Deleting old backups"
+    find ${BACKUP_DIR} -type f -mtime +${DELETE_AFTER} -name "${ODOO_DATABASE}.*.zip" -delete
+fi
+
+# If we want to restore the database as well
+if [ ! -z "$RESTORE_URL" ] ; then
+    restore_web_backup
+fi
+
+if [ ! -z "$RESTORE_DB" ] ; then
+    restore_db_backup
 fi
